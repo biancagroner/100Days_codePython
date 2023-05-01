e99fc11c62e2cc267df8300086e2fcc9b66973a5
bianca.xavier <bianca.groner@hotmail.com>
2023-05-01 16:32:17 -0300

    Day1- criando e imprimindo variáveis

---
+++ Day1/variaveis.py
@@ -0,0 +1,14 @@
+print("1-Criancando váriaveis e imprimindo uma frase com essas variáveis")
+
+nome = input("Escreva o seu nome: ")
+idade = input("Digite a sua idade: ")
+animal_estimacao = input("Digite o nome do seu animal de estimação: ")
+
+print("Seu nome é",nome,"e tem",idade,"anos. O nome do seu animal de estimação é:",animal_estimacao)
+
+print("##############################################################################################")
+
+print("2-Vendo o tamanha de uma variável")
+
+nome = input("Digite o seu nome e veja o tamanho dele: ")
+print(len(nome))
